5db1be3ae324364695d823fdb0acfd1bfe6e8848
Uni-T17 <mintayzamt17@gmail.com>
2025-08-17 16:06:48 +0700

    add rate limit

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import compression from "compression";
 import morgan from "morgan";
 import cors from "cors";
 
+import { limiter } from "./middlewears/rateLimit";
+
 export const app = express();
 
 // app.use(morgan("dev")); // Give log for every api usage from frontend
@@ -12,10 +14,21 @@ export const app = express();
 
 // app.use(express.json()); // From string json to pure json
 
-// app.use(cors()); // cros = Cross Origin Resourse sharing
+// app.use(cors()); // cors = Cross(Not same like fe port and be port) Origin(means port,domain) Resourse(Data) sharing
+
+// app.use(helmet()); // to more secure like don't allow browser to guess the file name
+
+// app.use(compression()); // to compress zip file for faster response but use more CPU(can ignore)
 
 app
   .use(morgan("dev"))
   .use(express.urlencoded({ extended: true }))
   .use(express.json())
-  .use(cors());
+  .use(cors())
+  .use(helmet())
+  .use(compression())
+  .use(limiter);
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Okay This is my reply!" });
+});
