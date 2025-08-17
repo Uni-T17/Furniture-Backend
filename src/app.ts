5997b6bf83dbb48827f01aa84888cfd65cb0f05c
Uni-T17 <mintayzamt17@gmail.com>
2025-08-17 13:23:14 +0700

    Add use

--- src/app.ts
+++ src/app.ts
@@ -6,4 +6,16 @@ import cors from "cors";
 
 export const app = express();
 
-app.use(morgan("dev")); // Give log for every api usage from frontend
+// app.use(morgan("dev")); // Give log for every api usage from frontend
+
+// app.use(express.urlencoded({ extended: true })); // To get direct access like req.body.password like that
+
+// app.use(express.json()); // From string json to pure json
+
+// app.use(cors()); // cros = Cross Origin Resourse sharing
+
+app
+  .use(morgan("dev"))
+  .use(express.urlencoded({ extended: true }))
+  .use(express.json())
+  .use(cors());
