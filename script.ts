cc633b1a2fcaec5bf7b60976e845b9a718c53281
Uni-T17 <mintayzamt17@gmail.com>
2025-08-26 15:08:04 +0700

    Done - Model for User and Post

--- script.ts
+++ script.ts
@@ -4,8 +4,8 @@ const prisma = new PrismaClient();
 
 async function main() {
   // ... you will write your Prisma Client queries here
-  const users = await prisma.user.findMany();
-  console.log(users);
+  const allUsers = await prisma.user.findMany();
+  console.log(allUsers);
 }
 
 main()
