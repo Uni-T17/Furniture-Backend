8d44497a4db4c4b6b0e8a799e3abf029ece48f0f
Uni-T17 <mintayzamt17@gmail.com>
2025-08-28 16:46:59 +0700

    Data Seeding (Scripting)

--- script.ts
+++ script.ts
@@ -1,19 +1,50 @@
-import { PrismaClient } from "./generated/prisma";
+import { Prisma, PrismaClient } from './generated/prisma'
+import * as bcript from "bcrypt";
 
 const prisma = new PrismaClient();
 
+const userData: Prisma.UserCreateInput[] = [
+  {
+    phone: "0999832340",
+    password: "",
+    randToken : "soejoijeoi201fowo"
+  },{
+    phone: "0999832341",
+    password: "",
+    randToken : "soejoijeoi201fowo"
+  },{
+    phone: "0999832342",
+    password: "",
+    randToken : "soejoijeoi201fowo"
+  },{
+    phone: "0999832343",
+    password: "",
+    randToken : "soejoijeoi201fowo"
+  },{
+    phone: "0999832344",
+    password: "",
+    randToken : "soejoijeoi201fowo"
+  }
+]
+
 async function main() {
-  // ... you will write your Prisma Client queries here
-  const allUsers = await prisma.user.findMany();
-  console.log(allUsers);
+  console.log("Scripting is starting...");
+  const salt = await bcript.genSalt(10);
+  const password = await bcript.hash("1234567", salt);
+  for(const user of userData) {
+    user.password = password;
+    await prisma.user.create(
+      {
+        data: user
+      }
+    )
+  }
+  console.log("Scripting done");
 }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
+main().then(async () =>
+  await prisma.$disconnect()).catch(async (e) => {
+    console.log(e);
     await prisma.$disconnect();
     process.exit(1);
-  });
+  })
