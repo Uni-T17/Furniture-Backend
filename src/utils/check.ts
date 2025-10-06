331ab4e1a549ccf8f2f77a945beda82d9dab1dbf
Uni-T17 <mintayzamt17@gmail.com>
2025-10-06 23:19:15 +0700

    Done

--- src/utils/check.ts
+++ src/utils/check.ts
@@ -2,7 +2,7 @@ import { createError, errorCode } from "./error";
 
 export const checkFileNotExist = (file: any) => {
   if (!file) {
-    const error = createError("Invalid File!", 404, errorCode.invalid);
+    const error = createError("Invalid File!!!", 404, errorCode.invalid);
     throw error;
   }
 };
