9685ee7f1739bff68925045e7751a70578521f85
Uni-T17 <mintayzamt17@gmail.com>
2025-10-09 22:41:03 +0700

    Done

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ import "dotenv/config";
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running!! on ${PORT}`);
+  console.log(`Server is running!!! on ${PORT}`);
 });
